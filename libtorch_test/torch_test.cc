ffb058e568ac92cc2c86589c2dc82801be452970
Akira Kawata <akirakawata1@gmail.com>
2023-07-02 23:12:17 +0000

    Test on GPU

--- libtorch_test/torch_test.cc
+++ libtorch_test/torch_test.cc
@@ -23,4 +23,17 @@ extern "C" void test() {
     auto r2 = at::convolution(x, w, c10::nullopt, c10::IntArrayRef({1, 1}), c10::IntArrayRef({0}), c10::IntArrayRef({1, 1}), false,
                               c10::IntArrayRef({0}), 1);
     SLOG() << SSHOW(r2.toString()) << std::endl;
+
+    SLOG() << "matrix multiplication test" << std::endl;
+    auto a1 = at::randn({256, 256}, at::kFloat);
+    auto a2 = at::randn({256, 256}, at::kFloat);
+    auto b = at::mm(a1, a2);
+
+    SLOG() << "matrix multiplication on GPU test" << std::endl;
+    at::Device device("cuda:0");
+    a1 = a1.to(device);
+    a2 = a2.to(device);
+    b = at::mm(a1, a2);
+
+    SLOG() << "All tests finished." << std::endl;
 }
