35ecf8e74fd5d5f0be4752af87d7d010f2429683
Zijian Xu <joe@preferred.jp>
2023-07-10 13:35:10 +0900

    Separate tests

--- .pfnci/run_gpu_test_all.sh
+++ .pfnci/run_gpu_test_all.sh
@@ -2,11 +2,8 @@
 set -eux
 
 for LIBTORCH in cu113/1.12.1 cu117/1.13.0; do
-    for OS in ubuntu20.04 ubuntu22.04; do
-        IFS=/ read -r LIBTORCH_ARCH LIBTORCH_VERSION <<< $LIBTORCH
-        LIBTORCH_VERSION=$LIBTORCH_VERSION \
-        LIBTORCH_ARCH=$LIBTORCH_ARCH \
-        OS=$OS \
-        bash .pfnci/run_gpu_test.sh
-    done
+    IFS=/ read -r LIBTORCH_ARCH LIBTORCH_VERSION <<< $LIBTORCH
+    LIBTORCH_VERSION=$LIBTORCH_VERSION \
+    LIBTORCH_ARCH=$LIBTORCH_ARCH \
+    bash .pfnci/run_gpu_test.sh
 done
