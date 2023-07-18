6184abae53b469a266a0f0df886c13f094281aef
Joe <joe@preferred.jp>
2023-07-18 08:24:35 +0000

    Make unzip quite

--- download-libtorch.sh
+++ download-libtorch.sh
@@ -9,4 +9,4 @@ LIBTORCH_ZIP_SUFFIX="%2B${LIBTORCH_ARCH}"
 URL="https://download.pytorch.org/libtorch${LIBTORCH_RELEASE_TYPE}/${LIBTORCH_ARCH}/libtorch${LIBTORCH_ABI}${LIBTORCH_OSNAME}${LIBTORCH_LIBOPT}-${LIBTORCH_VERSION}${LIBTORCH_ZIP_SUFFIX}.zip"
 
 wget -q -O libtorch.zip "${URL}"
-unzip libtorch.zip -d pytorch-install
+unzip -q libtorch.zip -d pytorch-install
