d49fe561331cb8b4ebdcedec0ac055e347908cb8
Akira Kawata <akirakawata@preferred.jp>
2024-07-24 22:01:31 +0900

    Fix

--- feature_unit_tests/user-defined-exception-g++/test.sh
+++ feature_unit_tests/user-defined-exception-g++/test.sh
@@ -1,10 +1,10 @@
 #! /bin/bash -eu
 
 g++ -fPIC -shared -o libfuga.so -Wl,-soname,libfuga.so fuga.cc 
+cp libfuga.so libfuga.so.original
 g++ main.cc -o main.out libfuga.so
 LD_LIBRARY_PATH=. $(git rev-parse --show-toplevel)/build/sold -i libfuga.so.original -o libfuga.so.soldout --section-headers --check-output
 
-cp libfuga.so libfuga.so.original
 # Use sold
 ln -sf libhoge.so.soldout libhoge.so
 # Use original
